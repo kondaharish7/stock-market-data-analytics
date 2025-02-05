067d0124977151f6f133100c5e21e87d85eaf32d
HarishK <kondaharish7@gmail.com>
2025-02-05 08:33:02 +0530

    05feb2025

--- smda_GetAllSectors.py
+++ smda_GetAllSectors.py
@@ -15,8 +15,8 @@ for a in base_url_html_page.findAll('a', class_="CardWeb_grayBoxStrip__4UAIy"):
     try:
         sector_url = f"{a['href']}"
         sector = a.find('span', class_="CardWeb_sectors_name__bryNo").text
-        market_cap = f"{a.find('span', class_="CardWeb_value__NLX7e CardWeb_font14____K3u").text}"
-        PE_Ratio = f"{a.find('span', class_="CardWeb_value__NLX7e CardWeb_mt5__d9MW6 CardWeb_font14____K3u").text}"
+        market_cap = a.find('span', class_="CardWeb_value__NLX7e CardWeb_font14____K3u").text
+        PE_Ratio = a.find('span', class_="CardWeb_value__NLX7e CardWeb_mt5__d9MW6 CardWeb_font14____K3u").text
 
         for b in a.findAll('span', class_="CardWeb_stocksNum__7tKf8"):
             if 'industries' in str(b.text).lower():
