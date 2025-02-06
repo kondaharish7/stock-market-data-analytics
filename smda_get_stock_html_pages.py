b003693097c1e0a2b1443f2d419434927a8b5bb7
HarishK <kondaharish7@gmail.com>
2025-02-06 19:49:20 +0530

    06feb2025

--- smda_get_stock_html_pages.py
+++ smda_get_stock_html_pages.py
@@ -4,11 +4,10 @@ job_start_time = datetime.now()
 s3_client = get_s3_client()
 
 def get_html_file(stock_url, stock_name) -> None:
-     stock_name = stock_name.replace(" ", "_")
     print(f"Saving html file for {stock_name}, ", end="");log_time = datetime.now()
     stock_url_response = requests.get(stock_url, timeout=2)
     stock_url_html_page = BeautifulSoup(stock_url_response.text, 'html.parser')
-    stocks_html_file_key = f"data/stocks_html_files/{}/{}.html".format(Sector, stock_name.replace(" ", "_"))
+    stocks_html_file_key = "data/stocks_html_files/{}/{}.html".format(Sector, stock_name.replace(" ", "_"))
     html_io_buffer = io.BytesIO(str(stock_url_html_page).encode('utf-8'))
     s3_client.put_object(Body=html_io_buffer, Bucket=aws_s3_bucket, Key=stocks_html_file_key)
     print(f"elapsed: {datetime.now() - log_time}")
@@ -27,16 +26,17 @@ if __name__ == '__main__':
                                  encoding='UTF-8'
                                  )
 
-    # df_all_sectors = pd.read_csv(all_sectors_s3_key_latest, storage_options={"key": root_user_access_key, "secret": root_user_sceret_key})
-    df_all_sectors = df_all_sectors[df_all_sectors['Sector'] == 'Banks']
+    df_all_sectors = df_all_sectors[df_all_sectors['Sector'] == 'Oil & Gas']
+    # print(df_all_sectors)
 
     # create a dataframe with all the stocks and their url's
     for index, row in df_all_sectors.iterrows():
         Sector = row.loc['Sector'].replace(" ", "_")
         print(Sector)
 
-        sector_stocks_list_s3_key = f"data/stocks_list/{}_stocks_list.csv".format(Sector)
+        sector_stocks_list_s3_key = "data/stocks_list/{}_stocks_list.csv".format(Sector)
         s3_file_resp = s3_client.get_object(Bucket=aws_s3_bucket, Key=sector_stocks_list_s3_key)
+        # print(s3_file_resp)
 
         # Create an in-memory buffer and write the CSV data into it
         sector_stocks_list_io_buffer = io.StringIO(s3_file_resp['Body'].read().decode('utf-8'))
@@ -54,20 +54,20 @@ if __name__ == '__main__':
             else:
                 pass
 
-    if len(failed_stocks_list) > 0:
-        print(f"Stocks failed:\ncount: {len(failed_stocks_list)}\nlist: {failed_stocks_list}")
-    while len(failed_stocks_list) > 0:
-        print(f"\nRe-pulling the html files for the failed stocks.")
-        failed_stocks_list1 = failed_stocks_list; failed_stocks_list = []
-        for stock_details in failed_stocks_list1:
-            try:
-                stock_url = stock_details[0];stock_name = stock_details[1]
-                get_html_file(stock_url=stock_url, stock_name=stock_name)
-            except Exception as get_html_err:
-                print(f"Timed Out.")
-                failed_stocks_list.append([stock_url, stock_name])
         if len(failed_stocks_list) > 0:
-            print(f"Stocks failed during re-pull\ncount: {len(failed_stocks_list)}\nlist: {failed_stocks_list}")
+            print(f"Stocks failed:\ncount: {len(failed_stocks_list)}\nlist: {failed_stocks_list}")
+        while len(failed_stocks_list) > 0:
+            print(f"\nRe-pulling the html files for the failed stocks.")
+            failed_stocks_list1 = failed_stocks_list; failed_stocks_list = []
+            for stock_details in failed_stocks_list1:
+                try:
+                    stock_url = stock_details[0];stock_name = stock_details[1]
+                    get_html_file(stock_url=stock_url, stock_name=stock_name)
+                except Exception as get_html_err:
+                    print(f"Timed Out.")
+                    failed_stocks_list.append([stock_url, stock_name])
+            if len(failed_stocks_list) > 0:
+                print(f"Stocks failed during re-pull\ncount: {len(failed_stocks_list)}\nlist: {failed_stocks_list}")
 
     print(f"\n{str('--')*10}\n{job_start_time} | {datetime.now()} | {datetime.now() - job_start_time}")
 
